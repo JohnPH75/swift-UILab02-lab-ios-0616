815f35f5f691ad807df3b175a41811fceca813f0
JohnPH75 <johnhussain75@gmail.com>
2016-07-11 14:17:08 -0400

    UILab02

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -11,17 +11,66 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Set up IB outlets
 
+    @IBOutlet weak var paintBucket: UIImageView!
+    
+    @IBOutlet weak var firstSegControl: UISegmentedControl!
+
+    @IBOutlet weak var secondSegControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Set the initial paint color to "red"
+        paintBucket.paintColorName = "red"
     }
 
     func mixColors(withFirst first: String, second: String) -> String {
         // TODO: Mix colors and return a string indicating the name of the mixed color
         // (e.g., "red", "purple", "blue")
+        
+        switch (first.lowercaseString, second.lowercaseString) {
+            
+        case ("yellow", "yellow"):
+            return "yellow"
+            
+        case ("blue", "blue"):
+            return "blue"
+            
+        case ("red", "yellow"):
+            return "orange"
+        case ("yellow", "red"):
+            return "orange"
+            
+        case ("red", "blue"):
+            return "purple"
+        case ("blue", "red"):
+            return "purple"
+            
+        case ("yellow", "blue"):
+            return "green"
+        case ("blue", "yellow"):
+            return "green"
+            
+        default:
+        return "red"
+        }
     }
 
-    @IBAction func colorSelected(sender: UISegmentedControl) {
+    func colorSelected(sender: UISegmentedControl) {
         // TODO: Mix each selected color and set the paint color to the mixed color
+        
+        var firstColorTapped = firstSegControl.color.name
+        var secondColorTapped = secondSegControl.color.name
+       
+        
+        if sender == firstSegControl {
+            firstColorTapped = sender.color.name
+        }
+        else {
+            secondColorTapped = sender.color.name
+        }
+        
+        
+        paintBucket.paintColorName = mixColors(withFirst: firstColorTapped, second: secondColorTapped)
     }
 }
